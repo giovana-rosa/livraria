85ecd1bec37a799dba26255510b2eb4d3d375261
giovana-rosa <giovana.rosaifc@gmail.com>
2026-06-06 02:03:28 -0300

    feat: criação da API para Autor

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,7 @@ from core.views import (
     UserViewSet,
     CategoriaViewSet,
     EditoraViewSet,
+    AutorViewSet,
 )
 
 router = DefaultRouter()
@@ -21,6 +22,7 @@ router = DefaultRouter()
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r'categorias', CategoriaViewSet, basename='categorias')
 router.register(r'editoras', EditoraViewSet, basename='editoras')
+router.register(r'autores', AutorViewSet, basename='autores')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
