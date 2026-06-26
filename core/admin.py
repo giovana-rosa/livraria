bf3784db0b442175a7acf9b84e1d04668f66a520
giovana-rosa <giovana.rosaifc@gmail.com>
2026-06-26 08:18:39 -0300

    feat: atualização no requirements.txt

--- core/admin.py
+++ core/admin.py
@@ -2,15 +2,14 @@
 Django admin customization.
 """
 
-from django.contrib import admin
+from django.contrib.admin import ModelAdmin, TabularInline, register
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
-from core import models
-from core.models import Autor, Categoria, Compra, Editora, Livro, User, ItensCompra
+from core.models import Autor, Categoria, Compra, Editora, ItensCompra, Livro, User
 
 
-@admin.register(User)
+@register(User)
 class UserAdmin(BaseUserAdmin):
     """Define the admin pages for users."""
 
@@ -53,46 +52,48 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
 
-@admin.register(Autor)
-class AutorAdmin(admin.ModelAdmin):
+@register(Autor)
+class AutorAdmin(ModelAdmin):
     list_display = ('nome', 'email')
     search_fields = ('nome', 'email')
     list_filter = ('nome',)
     ordering = ('nome', 'email')
     list_per_page = 10
 
-@admin.register(Categoria)
-class CategoriaAdmin(admin.ModelAdmin):
+@register(Categoria)
+class CategoriaAdmin(ModelAdmin):
     list_display = ('descricao',)
     search_fields = ('descricao',)
     list_filter = ('descricao',)
     ordering = ('descricao',)
     list_per_page = 10
 
-@admin.register(Editora)
-class EditoraAdmin(admin.ModelAdmin):
+@register(Editora)
+class EditoraAdmin(ModelAdmin):
     list_display = ('nome', 'email', 'cidade')
     search_fields = ('nome', 'email', 'cidade')
     list_filter = ('nome', 'email', 'cidade')
     ordering = ('nome', 'email', 'cidade')
     list_per_page = 10
 
-@admin.register(Livro)
-class LivroAdmin(admin.ModelAdmin):
+@register(Livro)
+class LivroAdmin(ModelAdmin):
     list_display = ('titulo', 'editora', 'categoria')
     search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
     list_filter = ('editora', 'categoria')
     ordering = ('titulo', 'editora', 'categoria')
     list_per_page = 25
 
-@admin.register(Compra)
-class CompraAdmin(admin.ModelAdmin):
+
+class ItensCompraInline(TabularInline):
+    model = ItensCompra
+    extra = 1  # Quantidade de itens adicionais
+
+@register(Compra)
+class CompraAdmin(ModelAdmin):
     list_display = ('usuario', 'status')
+    search_fields = ('usuario', 'status')
+    list_filter = ('usuario', 'status')
     ordering = ('usuario', 'status')
     list_per_page = 10
-
-@admin.register(ItensCompra)
-class ItensCompraAdmin(admin.ModelAdmin):
-    list_display = ('compra', 'livro', 'quantidade')
-    ordering = ('compra', 'livro')
-    list_per_page = 10
+    inlines = [ItensCompraInline]
