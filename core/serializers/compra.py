400ab0c72f31b0add8c549070b282cd0792fd06c
giovana-rosa <giovana.rosaifc@gmail.com>
2026-09-11 10:22:06 -0300

    feat: criação de uma compra a partir do usuário autenticado

--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import CharField, DecimalField, ModelSerializer, SerializerMethodField
+from rest_framework.serializers import CharField, DecimalField, ModelSerializer, SerializerMethodField, CurrentUserDefault, HiddenField
 from core.models import Compra, ItensCompra
 from django.db import transaction
 
@@ -33,6 +33,7 @@ class CompraSerializer(ModelSerializer):
 
 class CompraCreateUpdateSerializer(ModelSerializer):
     itens = ItensCompraCreateUpdateSerializer(many=True)
+    usuario = HiddenField(default=CurrentUserDefault())
 
     class Meta:
         model = Compra
@@ -46,6 +47,14 @@ class CompraCreateUpdateSerializer(ModelSerializer):
             for item in itens:
                 ItensCompra.objects.create(compra=compra, **item)
         return super().update(compra, validated_data)
+
+class ItensCompraListSerializer(ModelSerializer):
+    livro = CharField(source='livro.titulo', read_only=True)
+
+    class Meta:
+        model = ItensCompra
+        fields = ('quantidade', 'livro')
+        depth = 1
 
 class CompraListSerializer(ModelSerializer):
     usuario = CharField(source='usuario.email', read_only=True)
